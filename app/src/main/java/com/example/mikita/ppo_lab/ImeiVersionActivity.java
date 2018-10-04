6bab94ed472c36ab6c3b87dfc202d4a7094857af
Mikita Charnysh <mr.nick.cher@gmail.com>
2018-10-04 08:40:22 +0300

    activity created

---
+++ app/src/main/java/com/example/mikita/ppo_lab/ImeiVersionActivity.java
@@ -0,0 +1,13 @@
+package com.example.mikita.ppo_lab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImeiVersionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imei_version);
+    }
+}

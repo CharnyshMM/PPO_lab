ae5eb7378ee51c1809779d4cae814342e49a6eb5
Mikita Charnysh <mr.nick.cher@gmail.com>
2018-11-23 19:34:45 +0300

    basic bottom nav created

--- app/src/main/java/com/example/mikita/ppo_lab/MainActivity.java
+++ app/src/main/java/com/example/mikita/ppo_lab/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.mikita.ppo_lab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.net.Uri;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements ProfileFragment.OnFragmentInteractionListener,
+        NewsFragment.OnFragmentInteractionListener, FavoritesFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavController navController = Navigation.findNavController(this, R.id.my_nav_hos_f);
+        BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.main__bottom_navigation_view);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }

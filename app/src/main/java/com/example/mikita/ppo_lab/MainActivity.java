291fb1d3f7dbf538d19de5c9e170446e54c61446
Mikita Charnysh <mr.nick.cher@gmail.com>
2018-11-25 15:45:34 +0300

    started Firebase auth, (app/google-services.json is gitignored)

--- app/src/main/java/com/example/mikita/ppo_lab/MainActivity.java
+++ app/src/main/java/com/example/mikita/ppo_lab/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.mikita.ppo_lab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements ProfileFragment.OnFragmentInteractionListener,
         NewsFragment.OnFragmentInteractionListener, FavoritesFragment.OnFragmentInteractionListener,
         EditProfileFragment.OnFragmentInteractionListener
 {
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,41 @@ public class MainActivity extends AppCompatActivity implements ProfileFragment.O
         BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.main__bottom_navigation_view);
         NavigationUI.setupWithNavController(navigationView, navController);
 
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            //vse ok
+        } else {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), RC_SIGN_IN);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                finish();
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(this, "shit, why did you pressed back???", Toast.LENGTH_LONG);
+                    return;
+                }
+
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, "shit, NO NETWORK ERROR.", Toast.LENGTH_LONG);
+                    return;
+                }
+
+                Toast.makeText(this, "unknown error", Toast.LENGTH_LONG);
+
+                Log.e("MAIN ACTIVITY", "Sign-in error: ", response.getError());
+            }
+        }
     }
 
     @Override
